387c6ab229ae701f39d26c0245c6100de974fccb
Sander Elias <sanderelias+git@gmail.com>
2018-08-05 06:26:25 +0200

    added error handler, checked completion behaiour

--- src/app/obs-async-iter/obs-async-iter.component.ts
+++ src/app/obs-async-iter/obs-async-iter.component.ts
@@ -1,11 +1,6 @@
-import {
-  Component,
-  OnInit,
-  ViewChild,
-  ElementRef,
-  AfterViewInit
-} from '@angular/core';
-import { fromEvent, Observable } from 'rxjs';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { fromEvent, Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 if (Symbol['asyncIterator'] === undefined) {
   (Symbol as any)['asyncIterator'] = Symbol.for('asyncIterator');
@@ -16,8 +11,9 @@ if (Symbol['asyncIterator'] === undefined) {
   templateUrl: './obs-async-iter.component.html',
   styleUrls: ['./obs-async-iter.component.css']
 })
-export class ObsAsyncIterComponent implements OnInit {
+export class ObsAsyncIterComponent implements OnInit, OnDestroy {
   @ViewChild('btn') btnRef: ElementRef;
+  destroy$ = new Subject<void>();
   btn: HTMLButtonElement;
 
   constructor() {}
@@ -25,10 +21,15 @@ export class ObsAsyncIterComponent implements OnInit {
   async ngOnInit() {
     this.btn = this.btnRef.nativeElement;
     console.log(this.btn);
-    const clicks$ = fromEvent(this.btn, 'click');
+    const clicks$ = fromEvent(this.btn, 'click').pipe(takeUntil(this.destroy$));
     for await (const event of toAsyncIterator(clicks$)) {
-      console.log('click!!', event)
+      console.log('click!!', event);
     }
+    console.log('elemnt is destroyd');
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
   }
 }
 
@@ -38,20 +39,27 @@ interface Res {
 }
 
 async function* toAsyncIterator(ob: Observable<any>) {
-  let goResolve;
-  const setPromise = () => new Promise<Res>(resolve => (goResolve = resolve));
+  let goResolve, goReject;
+  const setPromise = () =>
+    new Promise<Res>((resolve, reject) => {
+      goResolve = resolve;
+      goReject = reject;
+    });
   let lastPromise = setPromise();
 
-  ob.subscribe(r =>
-    goResolve(
-      {
+  ob.subscribe(
+    r =>
+      goResolve({
         value: r,
         done: false
-      },
-      undefined, // TODO: add error/reject handling.
-      goResolve({ value: undefined, done: true })
-    )
+      }),
+    err => goReject(err),
+    () => goResolve({ value: undefined, done: true })
   );
+
+  // Still have to decide what to to with the error case
+  // for now, it will be forwarded to the consumer.
+  // (they need to implement a try/catch)
 
   while (true) {
     const result: Res = await lastPromise;
